c59947e5c651f0a5c163ea44da70b632061ba2ca
mavencoder <yusufalege@yahoo.com>
2023-10-21 14:15:58 +0000

    first commit

--- arsh_customization/hooks.py
+++ arsh_customization/hooks.py
@@ -2,7 +2,7 @@ from . import __version__ as app_version
 
 app_name = "arsh_customization"
 app_title = "Arsh Customization"
-app_publisher = "Optimum Solutions Ltd"
+app_publisher = "Optisol"
 app_description = "Ariosh Customization"
 app_email = "optisol.ltd@gmail.com"
 app_license = "MIT"
@@ -42,7 +42,7 @@ app_license = "MIT"
 
 # website user home page (by Role)
 # role_home_page = {
-#	"Role": "home_page"
+# "Role": "home_page"
 # }
 
 # Generators
@@ -56,8 +56,8 @@ app_license = "MIT"
 
 # add methods and filters to jinja environment
 # jinja = {
-#	"methods": "arsh_customization.utils.jinja_methods",
-#	"filters": "arsh_customization.utils.jinja_filters"
+# "methods": "arsh_customization.utils.jinja_methods",
+# "filters": "arsh_customization.utils.jinja_filters"
 # }
 
 # Installation
@@ -83,52 +83,54 @@ app_license = "MIT"
 # Permissions evaluated in scripted ways
 
 # permission_query_conditions = {
-#	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# "Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
 # }
 #
 # has_permission = {
-#	"Event": "frappe.desk.doctype.event.event.has_permission",
+# "Event": "frappe.desk.doctype.event.event.has_permission",
 # }
 
 # DocType Class
 # ---------------
 # Override standard doctype classes
 
-# override_doctype_class = {
-#	"ToDo": "custom_app.overrides.CustomToDo"
-# }
+override_doctype_class = {
+    # "ToDo": "custom_app.overrides.CustomToDo"
+    "Salary Slip": "arsh_customization.arsh_customization.salary_slip.ArioshSalarySlip",
+    "Attendance Request": "arsh_customization.arsh_customization.attendance.ArioshAttendanceRequest"
+}
 
 # Document Events
 # ---------------
 # Hook on document methods and events
 
 # doc_events = {
-#	"*": {
-#		"on_update": "method",
-#		"on_cancel": "method",
-#		"on_trash": "method"
-#	}
+# "*": {
+# "on_update": "method",
+# "on_cancel": "method",
+# "on_trash": "method"
+# }
 # }
 
 # Scheduled Tasks
 # ---------------
 
 # scheduler_events = {
-#	"all": [
-#		"arsh_customization.tasks.all"
-#	],
-#	"daily": [
-#		"arsh_customization.tasks.daily"
-#	],
-#	"hourly": [
-#		"arsh_customization.tasks.hourly"
-#	],
-#	"weekly": [
-#		"arsh_customization.tasks.weekly"
-#	],
-#	"monthly": [
-#		"arsh_customization.tasks.monthly"
-#	],
+# "all": [
+# "arsh_customization.tasks.all"
+# ],
+# "daily": [
+# "arsh_customization.tasks.daily"
+# ],
+# "hourly": [
+# "arsh_customization.tasks.hourly"
+# ],
+# "weekly": [
+# "arsh_customization.tasks.weekly"
+# ],
+# "monthly": [
+# "arsh_customization.tasks.monthly"
+# ],
 # }
 
 # Testing
@@ -140,14 +142,14 @@ app_license = "MIT"
 # ------------------------------
 #
 # override_whitelisted_methods = {
-#	"frappe.desk.doctype.event.event.get_events": "arsh_customization.event.get_events"
+# "frappe.desk.doctype.event.event.get_events": "arsh_customization.event.get_events"
 # }
 #
 # each overriding function accepts a `data` argument;
 # generated from the base implementation of the doctype dashboard,
 # along with any modifications made in other Frappe apps
 # override_doctype_dashboards = {
-#	"Task": "arsh_customization.task.get_dashboard_data"
+# "Task": "arsh_customization.task.get_dashboard_data"
 # }
 
 # exempt linked doctypes from being automatically cancelled
@@ -173,29 +175,29 @@ app_license = "MIT"
 # --------------------
 
 # user_data_fields = [
-#	{
-#		"doctype": "{doctype_1}",
-#		"filter_by": "{filter_by}",
-#		"redact_fields": ["{field_1}", "{field_2}"],
-#		"partial": 1,
-#	},
-#	{
-#		"doctype": "{doctype_2}",
-#		"filter_by": "{filter_by}",
-#		"partial": 1,
-#	},
-#	{
-#		"doctype": "{doctype_3}",
-#		"strict": False,
-#	},
-#	{
-#		"doctype": "{doctype_4}"
-#	}
+# {
+# "doctype": "{doctype_1}",
+# "filter_by": "{filter_by}",
+# "redact_fields": ["{field_1}", "{field_2}"],
+# "partial": 1,
+# },
+# {
+# "doctype": "{doctype_2}",
+# "filter_by": "{filter_by}",
+# "partial": 1,
+# },
+# {
+# "doctype": "{doctype_3}",
+# "strict": False,
+# },
+# {
+# "doctype": "{doctype_4}"
+# }
 # ]
 
 # Authentication and authorization
 # --------------------------------
 
 # auth_hooks = [
-#	"arsh_customization.auth.validate"
+# "arsh_customization.auth.validate"
 # ]
